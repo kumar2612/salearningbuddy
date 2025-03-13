aef27e1ee7865f7e568984a0c1c0cff10d363a5c
Kumar Ashish <kumarashish2612@gmail.com>
2025-03-13 19:01:50 +0530

    Initial commit of backend spring boot project

---
+++ src/main/java/com/learning/salearningbuddy/SalearningbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.learning.salearningbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalearningbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalearningbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/salearningbuddy/SalearningbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.salearningbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalearningbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
